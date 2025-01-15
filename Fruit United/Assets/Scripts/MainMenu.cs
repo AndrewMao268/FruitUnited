ff3c55de7265bb1636186d7c1e39a4c0d519f956
BB8377 <brandon883777@gmail.com>
2025-01-14 21:34:42 -0500

    Settings Thing
    hopefully I did this right

--- Fruit United/Assets/Scripts/MainMenu.cs
+++ Fruit United/Assets/Scripts/MainMenu.cs
@@ -1,8 +1,32 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public GameObject settingsMenu; // (By the way, CHATGPT is awesome..but still very dumb - Brandon)
+    public Button settingsButton; 
+    public Button closeButton;
+
+    void Start()
+    {
+        // Ensure the settings menu is hidden when the game starts
+        settingsMenu.SetActive(false);
+
+        // Add a listener to the button to call ShowSettingsMenu when clicked
+        settingsButton.onClick.AddListener(ShowSettingsMenu);
+        closeButton.onClick.AddListener(CloseSettingsMenu);
+    }
+    void CloseSettingsMenu()
+    {
+        settingsMenu.SetActive(false);
+    }
+    void ShowSettingsMenu()
+    {
+        // Toggle the visibility of the settings menu
+        settingsMenu.SetActive(!settingsMenu.activeSelf);
+    }
    public void StartGame()
    {
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
