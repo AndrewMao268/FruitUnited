63b723421e4059b0c506772df566f6a699455444
CometStorm991 <cometstorm991@gmail.com>
2025-03-16 21:09:33 -0400

    Finished clock display

--- Fruit United/Assets/Scripts/Lighting/Clock.cs
+++ Fruit United/Assets/Scripts/Lighting/Clock.cs
@@ -1,16 +1,22 @@
+using TMPro;
 using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
+    public GameObject daylightCycleObj;
+
+    private TMP_Text text;
+    private DaylightCycle daylightCycle;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        text = GetComponent<TMP_Text>();
+        daylightCycle = daylightCycleObj.GetComponent<DaylightCycle>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        text.text = daylightCycle.GetTimeString;
     }
 }
