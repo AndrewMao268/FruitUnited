4efef8bb5e366a2bb3083b315dc72e5f7c2a5c04
AndrewMao268 <AndrewMao268@gmail.com>
2025-03-25 10:05:20 -0400

    Fixed health bar flipping

--- Fruit United/Assets/Scripts/HealthBar.cs
+++ Fruit United/Assets/Scripts/HealthBar.cs
@@ -11,8 +11,9 @@ public class HealthBar : MonoBehaviour
 
     private Vector3 originalPosition;
     public GameObject allHealthBar;
+    public GameObject player;
 
-
+    private Vector3 offset;
 
 
 
@@ -21,13 +22,18 @@ public class HealthBar : MonoBehaviour
         currentHealth = maxHealth;
         originalPosition = mask.localPosition;
         targetPosition = originalPosition;
+
+        offset = new Vector3(-5.570001f, 3.46f, 0);
     }
 
     void Update()
     {
 
 
-        allHealthBar.transform.localScale = new Vector3(1, 1, 1);
+        allHealthBar.transform.position = player.transform.position + offset;
+
+
+
         // Smoothly move the health bar's position towards the target position
         mask.localPosition = Vector3.Lerp(mask.localPosition, targetPosition, 5f * Time.deltaTime);
 
